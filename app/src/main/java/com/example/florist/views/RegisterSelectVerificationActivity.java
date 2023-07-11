4a0ba263a2773ca27efdf43e34c10a7677f29482
Administrator <fajar.wira1403@gmail.com>
2023-07-11 18:00:03 +0700

    [View]RegisterSelectVerificationActivity & [View]RegisterVerificationActivity

--- app/src/main/java/com/example/florist/views/RegisterSelectVerificationActivity.java
+++ app/src/main/java/com/example/florist/views/RegisterSelectVerificationActivity.java
@@ -4,29 +4,47 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.florist.R;
 
 public class RegisterSelectVerificationActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_select_verification);
 
+        progressBar = findViewById(R.id.progressBar);
+
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeAsUpIndicator(R.drawable.arrow_back);
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
+
+        progressBar.setMax(1000);
+        int currentProgress = 500;
+        ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
+                .setDuration(1500)
+                .start();
+
+
+
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        Intent intent = new Intent(getApplicationContext(), RegisterSelectVerificationActivity.class);
         startActivityForResult(intent, 0);
         return true;
     }
---
+++ app/src/main/java/com/example/florist/views/RegisterVerificationActivity.java
@@ -0,0 +1,157 @@
+package com.example.florist.views;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.florist.R;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegisterVerificationActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    EditText editText1, editText2, editText3, editText4, editText5;
+    TextView countDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_verification);
+
+        progressBar = findViewById(R.id.progressBar);
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editText3);
+        editText4 = findViewById(R.id.editText4);
+        editText5 = findViewById(R.id.editText5);
+        countDownTimer = findViewById(R.id.countDownTimer);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.arrow_back);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
+
+        progressBar.setMax(1000);
+        int currentProgress = 750;
+        ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
+                .setDuration(1500)
+                .start();
+
+        setUpInputOTPInputs();
+        new CountDownTimer(120000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+
+                countDownTimer.setText(String.format("%02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(l) % TimeUnit.HOURS.toMinutes(1),
+                        TimeUnit.MILLISECONDS.toSeconds(l) % TimeUnit.MINUTES.toSeconds(1)));
+            }
+
+            @Override
+            public void onFinish() {
+                countDownTimer.setText("Done");
+            }
+        }.start();
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivityForResult(intent, 0);
+        return true;
+    }
+    private void setUpInputOTPInputs() {
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int count, int after) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    editText2.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int count, int after) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    editText3.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editText3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int count, int after) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    editText4.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editText4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int count, int after) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    editText5.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
